0ef43f6f51a607b8ffe037824e38646247b216f9
Braun,Naomi <nbraun@ctsi.ufl.edu>
2023-03-31 14:18:54 -0400

    Update midas version of river

--- static/js/riverStats.js
+++ static/js/riverStats.js
@@ -4,7 +4,6 @@ const riverStats = {
 }
 
 const getFilteredSet = (dataset) => {
-    console.log('we are here')
     rollup = d3.nest()
                .key((d) => {return d.topic;})
                .rollup(v => d3.sum(v,d => d.count), d => d.topic)
@@ -12,6 +11,25 @@ const getFilteredSet = (dataset) => {
                .sort((a,b) => {return d3.descending(a.value, b.value)})
 
     console.log(rollup)
-    return rollup.filter((d, i) => {return i < 20})
+    // return rollup.filter((d, i) => {return i < 20})
+    return rollup
 }
 
+const rebalanceSet = (dataset, sorting_set, up_max) => {
+    for (year of dataset){
+        year.sort((a,b) => sorting_set.indexOf(a['topic']) - sorting_set.indexOf(b['topic']));
+
+        let total = 0;
+        for (term of year){
+            total += term['count'];
+        }
+        
+        for (term of year){
+            term['raw_percent'] = (term['count']/total || 0);
+            term['year_total'] = total;
+            term['adjusted_percent'] = Math.round((term['raw_percent']*(total/up_max) || 0) * 100);
+        }
+    }
+
+    return dataset;
+}
